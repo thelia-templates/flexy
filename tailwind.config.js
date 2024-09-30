8b2bc97693f10f0ee7e9ab7ee7a96173dcc8568d
Achille Jouguelet <ajouguelet@openstudio.fr>
2024-09-30 16:11:29 +0200

    submenu mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,77 +5,77 @@ module.exports = {
     './form/**/*.twig',
     './*.twig'
   ],
-  theme  : {
-    extend  : {
-      colors : {
-        'theme-dark'          : 'var(--theme-dark)',
-        theme                 : 'var(--theme)',
-        'theme-medium'        : 'var(--theme-medium)',
-        'theme-light'         : 'var(--theme-light)',
-        'theme-lighter'       : 'var(--theme-lighter)',
-        'theme-lightest'      : 'var(--theme-lightest)',
-        black                 : 'var(--black)',
-        white                 : 'var(--white)',
-        'grey-darker'         : 'var(--grey-darker)',
-        'grey-dark'           : 'var(--grey-dark)',
-        grey                  : 'var(--grey)',
-        'grey-lighter'        : 'var(--grey-lighter)',
-        'grey-lightest'       : 'var(--grey-lightest)',
-        'error-dark'          : 'var(--error-dark)',
-        error                 : 'var(--error)',
-        'error-lighter'       : 'var(--error-lighter)',
-        'error-lightest'      : 'var(--error-lightest)',
-        'success-dark'        : 'var(--success-dark)',
-        success               : 'var(--success)',
-        'success-lightest'    : 'var(--success-lightest)',
-        warning               : 'var(--warning)',
-        'warning-lightest'    : 'var(--warning-lightest)',
-        validated             : 'var(--validated)',
-        'validated-lightest'  : 'var(--validated-lightest)',
-        informative           : 'var(--informative)',
+  theme: {
+    extend: {
+      colors: {
+        'theme-dark': 'var(--theme-dark)',
+        theme: 'var(--theme)',
+        'theme-medium': 'var(--theme-medium)',
+        'theme-light': 'var(--theme-light)',
+        'theme-lighter': 'var(--theme-lighter)',
+        'theme-lightest': 'var(--theme-lightest)',
+        black: 'var(--black)',
+        white: 'var(--white)',
+        'grey-darker': 'var(--grey-darker)',
+        'grey-dark': 'var(--grey-dark)',
+        grey: 'var(--grey)',
+        'grey-lighter': 'var(--grey-lighter)',
+        'grey-lightest': 'var(--grey-lightest)',
+        'error-dark': 'var(--error-dark)',
+        error: 'var(--error)',
+        'error-lighter': 'var(--error-lighter)',
+        'error-lightest': 'var(--error-lightest)',
+        'success-dark': 'var(--success-dark)',
+        success: 'var(--success)',
+        'success-lightest': 'var(--success-lightest)',
+        warning: 'var(--warning)',
+        'warning-lightest': 'var(--warning-lightest)',
+        validated: 'var(--validated)',
+        'validated-lightest': 'var(--validated-lightest)',
+        informative: 'var(--informative)',
         'informative-lightest': 'var(--informative-lightest)',
-        processing            : 'var(--processing)',
-        'processing-lightest' : 'var(--processing-lightest)',
-        'checkbox-focus'      : 'var(--checkbox-focus)'
+        processing: 'var(--processing)',
+        'processing-lightest': 'var(--processing-lightest)',
+        'checkbox-focus': 'var(--checkbox-focus)'
       },
       screens: {
-        xxs   : '320px',
-        '3xl' : '1920px',
+        xxs: '320px',
+        '3xl': '1920px',
         max2xl: { max: '1680px' },
-        maxXl : { max: '1366px' },
-        maxLg : { max: '1024px' },
-        maxMd : { max: '768px' },
-        maxSm : { max: '390px' },
-        maxXs : { max: '321px' }
+        maxXl: { max: '1366px' },
+        maxLg: { max: '1024px' },
+        maxMd: { max: '768px' },
+        maxSm: { max: '390px' },
+        maxXs: { max: '321px' }
       }
     },
-    screens : {
-      xs   : '390px',
-      sm   : '640px',
-      md   : '768px',
-      lg   : '1024px',
-      xl   : '1366px',
+    screens: {
+      xs: '390px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1366px',
       '2xl': '1680px'
     },
     fontSize: {
       '2xs': 'var(--font-size-2xs)',
-      xs   : 'var(--font-size-xs)',
-      sm   : 'var(--font-size-sm)',
-      base : 'var(--font-size-base)',
-      lg   : 'var(--font-size-lg)',
-      xl   : 'var(--font-size-xl)',
+      xs: 'var(--font-size-xs)',
+      sm: 'var(--font-size-sm)',
+      base: 'var(--font-size-base)',
+      lg: 'var(--font-size-lg)',
+      xl: 'var(--font-size-xl)',
       '2xl': 'var(--font-size-2xl)',
       '3xl': 'var(--font-size-3xl)',
       '4xl': 'var(--font-size-4xl)'
     }
   },
   plugins: [
-    function({ addComponents }) {
+    function ({ addComponents }) {
       addComponents({
-        '.container'       : {
-          maxWidth    : '272px',
-          marginLeft  : 'auto',
-          marginRight : 'auto',
+        '.container': {
+          maxWidth: '272px',
+          marginLeft: 'auto',
+          marginRight: 'auto',
           '@screen xs': {
             maxWidth: 'calc(100% - 56px)'
           },
@@ -93,7 +93,7 @@ module.exports = {
           }
         },
         '.container-medium': {
-          maxWidth    : '100%',
+          maxWidth: '100%',
           '@screen xs': {
             maxWidth: '272px'
           },
@@ -104,8 +104,8 @@ module.exports = {
             maxWidth: '540px'
           }
         },
-        '.container-small' : {
-          maxWidth    : '100%',
+        '.container-small': {
+          maxWidth: '100%',
           '@screen xs': {
             maxWidth: '272px'
           },
