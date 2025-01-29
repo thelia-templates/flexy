9e374902f3366462786c48f14bcb8bb6b9263662
lnormandon <lnormandon@openstudio.fr>
2025-01-29 16:45:23 +0100

    use api instead of controller twig for delivery modules listing

--- src/Service/ModuleService.php
+++ src/Service/ModuleService.php
@@ -25,68 +25,21 @@ use Thelia\Tools\URL;
 
 class ModuleService
 {
-    public function __construct(protected readonly ContainerInterface $container, protected readonly Session $session)
-    {
-    }
+  public function __construct(protected readonly ContainerInterface $container, protected readonly Session $session)
+  {
+  }
 
-    public function getModuleList(string $moduleType = null, int $area = null): array|ObjectCollection
-    {
-        $search = ModuleQuery::create();
+  public function getModuleLogoUrl(Module $module, $region = 'full', $size = '%5E*!40,40')
+  {
+    $imageId = $module->getModuleImages()->getFirst()?->getId();
 
-        if (null !== $area) {
-            $search
-              ->useAreaDeliveryModuleQuery()
-              ->filterByAreaId($area, Criteria::IN)
-              ->endUse();
-        }
+    return URL::getInstance()->absoluteUrl('legacy-image-library' . DS . 'module_image_' . $imageId . DS . $region . DS . $size . DS . '0' . DS . 'default.webp');
+  }
 
-        if (null !== $moduleType) {
-            $search->filterByType($moduleType, Criteria::IN);
-        }
+  public function getDeliveryModuleList(): array
+  {
+    $modules = $this->getModuleList(moduleType: BaseModule::DELIVERY_MODULE_TYPE);
 
-        $search->filterByActivate(1);
-
-        return $search->find();
-    }
-
-    /**
-     * @param array|ObjectCollection|Module[] $modules
-     */
-    protected function getModuleDeliveryByModes(array|ObjectCollection $modules): array
-    {
-        $list = [];
-        foreach ($modules as $module) {
-            /** @var AbstractDeliveryModule $moduleInstance */
-            $moduleInstance = $module->getDeliveryModuleInstance($this->container);
-            $mode = $moduleInstance->getDeliveryMode();
-            if (!isset($list[$mode])) {
-                $list[$mode] = [];
-            }
-            $locale = $this->session->getLang(true)->getLocale();
-            $module->setLocale($locale);
-
-            $list[$mode][] = [
-              ...$module->toArray(TableMap::TYPE_CAMELNAME),
-              'title' => $module->getTitle(),
-              'mode' => $mode,
-              'img' => $this->getModuleLogoUrl($module),
-            ];
-        }
-
-        return $list;
-    }
-
-    public function getModuleLogoUrl(Module $module, $region = 'full', $size = '%5E*!40,40')
-    {
-        $imageId = $module->getModuleImages()->getFirst()?->getId();
-
-        return URL::getInstance()->absoluteUrl('legacy-image-library'.DS.'module_image_'.$imageId.DS.$region.DS.$size.DS.'0'.DS.'default.webp');
-    }
-
-    public function getDeliveryModuleList(): array
-    {
-        $modules = $this->getModuleList(moduleType: BaseModule::DELIVERY_MODULE_TYPE);
-
-        return $this->getModuleDeliveryByModes($modules);
-    }
+    return $this->getModuleDeliveryByModes($modules);
+  }
 }
